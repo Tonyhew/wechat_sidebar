cf8aa5ac70d4084e4ac3423a4872de0ad8e64c95
Tonyhew <916390127@qq.com>
2022-02-18 14:03:19 +0800

    api接口公共配置文件

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,11 +1,12 @@
 // 配置公共BaseURL
-let url = '';
+let url = 'https://backstage.huangzhemin.com/miniapp';
 
 // 接口配置输出总览
 let servicePath = {
   // 
-  getTken: url + '',
-
+  getSignature: `${url}/signature/getSignature?htmlUrl=`,
+  // 获取用户信息
+  getUserList: `${url}/`
 };
 
 export default servicePath;
