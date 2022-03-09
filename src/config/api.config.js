30f430eae3efa685ec9118bd93840918221e8cfe
Tonyhew <916390127@qq.com>
2022-03-09 10:45:41 +0800

    api接口更改及其他

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -3,10 +3,11 @@ let url = 'https://backstage.huangzhemin.com/miniapp';
 
 // 接口配置输出总览
 let servicePath = {
-  // 
+  // 获取 agentConfig 配置信息（timestamp, nonceStr, signature）
   getSignature: `${url}/signature/getSignature?htmlUrl=`,
   // 获取用户信息
-  getUserList: `${url}/`
+  getUserInfo: `${url}/customerInformation/getCustomerDetails?userId=`,
+
 };
 
 export default servicePath;
--- src/config/setupProxy.js
+++ src/config/setupProxy.js
@@ -2,7 +2,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware'
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/cgi-bin/gettoken', {
+    createProxyMiddleware('/cgi-bin/getcode', {
       target: 'https://qyapi.weixin.qq.com',
       changeOrigin: true,
     }),
