fe5d4627c51132d0d6381d343ee95d7fcda418f0
Tonyhew <916390127@qq.com>
2022-03-09 10:29:55 +0800

    新增骨架屏，去除部分mock数据，新增手机号当中四位隐藏

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,39 +1,16 @@
+/**
+ * @description 用户信息组件，展示用户基本信息
+ * @author Tonyhew
+ */
+
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Card, Avatar, Select, Tag, Cascader } from 'antd';
+import { Card, Avatar, Select, Tag, Cascader, Skeleton } from 'antd';
 import { FormOutlined } from '@ant-design/icons';
-import * as ww from '@wecom/jssdk'
-import api from '../config/api.config';
 import '../assets/style/UserInfo.scss';
 
-function UserInfo() {
+function UserInfo(props) {
 
-  const data = [
-    {
-      id: 1,
-      isSuccess: true,
-      user: {
-        avatar: 'https://joeschmoe.io/api/v1/random',
-        name: 'tt',
-        mobile: 'xxxxxxxxxxx',
-        gender: '女'
-      }
-    }
-  ]
-
-  const clientS = [
-    {
-      id: 1,
-      source: '手动添加',
-      isSuccess: true
-    }
-  ]
-
-  const isH = [
-    {
-      value: '未到院',
-    }
-  ]
+  const qwUserList = props.qwUserList;
 
   const tagData = [
     {
@@ -77,10 +54,8 @@ function UserInfo() {
 
   const { Meta } = Card;
 
-  // 客户详细信息（头像、姓名、手机号）
-  const [wxUserList, setWxUserList] = useState([]);
-  // 客户来源
-  const [clientSource, setClientSource] = useState([]);
+  const [userListSkeleton, setUserListSkeleton] = useState(true)
+
   // 到院情况（字符串）
   const [isHospital, setIshospital] = useState('');
   // 客户标签
@@ -91,21 +66,19 @@ function UserInfo() {
 
   useEffect(() => {
 
-    getWXUserData()
+    if (qwUserList.length !== 0 && userListSkeleton) {
+      setUserListSkeleton(false)
+    }
 
-    setWxUserList(data);
+    // 客户标签
     setClientTag(tagData);
-    setClientSource(clientS);
-    setIshospital(isH[0].value);
+    // 是否已到院
+    setIshospital(qwUserList.visitStatus);
     // setHospitalList(hlList.list);
-    handleTagChange(chooseTag)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chooseTag, isHospital])
+    handleTagChange(chooseTag);
 
-  // mock客户数据
-  const getWXUserData = (data) => {
-    
-  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chooseTag, isHospital, qwUserList, userListSkeleton]);
 
   // 编辑客户信息
   const handleEdit = (e) => {
@@ -137,98 +110,146 @@ function UserInfo() {
   // 获取已被选中的客户标签（数组）
   const handleTagChange = (tagArr) => {
     setChooseTag(tagArr)
-    console.log(chooseTag)
   }
+
+
+  /**
+   * 隐藏手机号，当中四位为 * 。
+   * @param {string} mobileNum 默认值为用户信息的手机号，但也可以根据传入的参数更改
+   * @returns 一个隐藏的手机号
+   */
+  const hideMobileNum = (mobileNum = qwUserList.mobile) => {
+    let result = mobileNum.replace(/^(\d{3})\d+(\d{4})$/, '$1****$2')
+    return result;
+  }
+
+
 
   return (
     <>
 
-      <Card
-        bordered={false}
-        className="userInfo-list"
+      <Skeleton
+        active
+        avatar={{
+          shape: 'square',
+          size: 'large'
+        }}
+        loading={userListSkeleton}
       >
-        {
-          wxUserList.map((wxUserList, index) => {
-            return (
-              wxUserList.isSuccess ?
-                <div key={index} className={'userInfo-left'}>
-                  <Meta
-                    avatar={<Avatar src={wxUserList.user.avatar} />}
-                    title={
-                      <h2 className={'user-name'}>{wxUserList.user.name}</h2>
-                    }
-                    description={
-                      <>
-                        <div>
-                          <span className={'user-mobile'}>电话: {wxUserList.user.mobile}</span>
-                          <span className={'user-gender'}>性别: {wxUserList.user.gender}</span>
-                        </div>
-                      </>
-                    }
-                  />
+        <Card
+          bordered={false}
+          className="userInfo-list"
+        >
+          <div className={'userInfo-left'}>
 
-                </div> : null
-            )
-          })
-        }
+            <Meta
+              avatar={<Avatar src={qwUserList.avatar} />}
+              title={
+                <h2 className={'user-name'}>{qwUserList.name}</h2>
+              }
+              description={
+                <>
+                  <div>
+                    <span className={'user-mobile'}>电话: {hideMobileNum()}</span>
+                    <span className={'user-gender'}>性别: {qwUserList.gender}</span>
+                  </div>
+                </>
+              }
+            />
 
-        <div className={'userInfo-right'}>
-          <span className={'user-edit'} onClick={handleEdit}>
-            <FormOutlined />
-          </span>
+          </div>
 
-          <span className={'user-sayhi'} onClick={handleChat}>
-            打招呼
-          </span>
-        </div>
-
-      </Card>
-
-      <div className={'clientInfo'}>
-
-        <span className={'cTag'}>
-          客户来源
-          <p className={'clientSource tagSelect'}>
+          <div className={'userInfo-right'}>
             {
-              clientSource.map((item, index) => {
-                return (
-                  <span key={index}>{item.source}</span>
-                )
-              })
+              userListSkeleton ?
+                <Skeleton.Button
+                  active
+                  shape={'circle'}
+                  size={'small'}
+                  className={'user-edit'}
+                ></Skeleton.Button>
+                :
+                <span
+                  className={'user-edit'}
+                  onClick={handleEdit}
+                >
+                  <FormOutlined />
+                </span>
             }
-          </p>
-        </span>
 
-        <span className={'cTag'}>
-          到院情况
-          <Select
-            disabled
-            showArrow={false}
-            bordered={false}
-            value={isHospital}
-            className={'isHospital tagSelect'}
-          />
-        </span>
+            {
+              userListSkeleton ?
+                <Skeleton.Button
+                  active
+                  shape={'circle'}
+                  size={'small'}
+                  className={'user-sayhi'}
+                ></Skeleton.Button>
+                :
+                <span
+                  className={'user-sayhi'}
+                  onClick={handleChat}
+                >
+                  打招呼
+                </span>
+            }
 
-        <span className={'cTag clientTag'}>
-          客户标签
-          <Cascader
-            showArrow
-            multiple
-            bordered={false}
-            allowClear={false}
-            options={clientTag}
-            tagRender={tagRender}
-            placeholder={'添加标签'}
-            onChange={handleTagChange}
-            className={'tagSelect tag-c'}
-            dropdownClassName={'client_tag_dropdown'}
-          />
-        </span>
+          </div>
 
-        <div style={{ 'clear': 'both' }}></div>
-      </div>
+        </Card>
+      </Skeleton>
 
+      <Skeleton
+        active
+        paragraph={{ rows: 3 }}
+        loading={userListSkeleton}
+      >
+        <div className={'clientInfo'}>
+
+          <span className={'cTag'}>
+            客户来源
+            <p className={'clientSource tagSelect'}>
+              <span>{qwUserList.middleClass}</span>
+              {/* {
+                clientSource.map((item, index) => {
+                  return (
+                    <span key={index}>{item.source}</span>
+                  )
+                })
+              } */}
+            </p>
+          </span>
+
+          <span className={'cTag'}>
+            到院情况
+            <Select
+              disabled
+              showArrow={false}
+              bordered={false}
+              value={isHospital !== '' && isHospital !== null ? isHospital : '未到院'}
+              className={'isHospital tagSelect'}
+            />
+          </span>
+
+          <span className={'cTag clientTag'}>
+            客户标签
+            <Cascader
+              showArrow
+              multiple
+              bordered={false}
+              allowClear={false}
+              options={clientTag}
+              tagRender={tagRender}
+              placeholder={'添加标签'}
+              onChange={handleTagChange}
+              className={'tagSelect tag-c'}
+              dropdownClassName={'client_tag_dropdown'}
+            />
+          </span>
+
+          <div style={{ 'clear': 'both' }}></div>
+        </div>
+      </Skeleton>
     </>
   )
 
