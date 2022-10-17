f80172c56a2340cb62b28fc748423a4c6ad48019
tonyhew <916390127@qq.com>
2022-10-17 13:48:29 +0800

    如果没有消费记录,则显示 404 图片

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import { Timeline } from 'antd'
-import axios from 'axios'
-import api from '../config/api.config'
+import React, { useState, useEffect, useCallback } from 'react';
+import { Timeline, Result } from 'antd';
+import axios from 'axios';
+import api from '../config/api.config';
 // import '../assets/style/ClientProcess.scss'
 
 const timeLineData = [
@@ -15,14 +15,14 @@ const timeLineData = [
     time: '2021-09-05',
     event: 'ddddddddddddddd',
   },
-]
+];
 
 function Payment(props) {
-  const { crmId } = props
+  const { crmId } = props;
   // 时间线（时间及详细说明）
-  const [timeLine, setTimeLine] = useState([])
+  const [timeLine, setTimeLine] = useState([]);
   // 时间轴备注内容
-  const [tlRemark, setTLRemark] = useState('')
+  const [tlRemark, setTLRemark] = useState('');
 
   const getUserPayment = useCallback(() => {
     axios({
@@ -37,36 +37,40 @@ function Payment(props) {
         'Acess-Control-Allow-Origin': '*',
       },
     }).then((res) => {
-      console.log(res)
-      const data = res.data
-      setTimeLine(data.data)
-    })
-  }, [])
+      console.log(res);
+      const data = res.data;
+      setTimeLine(data.data);
+    });
+  }, []);
 
   useEffect(() => {
     // setTimeLine(timeLineData)
-    getUserPayment()
-  }, [getUserPayment])
+    getUserPayment();
+  }, [getUserPayment]);
 
   return (
     <div className={'timeline_main'} style={{ width: '90%', margin: '0 auto' }}>
-      <Timeline mode='left'>
-        {timeLine.map((item, index) => {
-          return (
-            <Timeline.Item key={index}>
-              <>
-                <p>{item.debitDate}</p>
-                <p>医生: {item.treatingDoctor}</p>
-                <p>手术名称: {item.treatmentMethod}</p>
-                <p>状态: {item.typeOfTutoring}</p>
-                <p>金额: {`￥${item.cash}`}</p>
-              </>
-            </Timeline.Item>
-          )
-        })}
-      </Timeline>
+      {timeLine.length > 0 ? (
+        <Timeline mode='left'>
+          {timeLine.map((item, index) => {
+            return (
+              <Timeline.Item key={index}>
+                <>
+                  <p>{item.debitDate}</p>
+                  <p>医生: {item.treatingDoctor}</p>
+                  <p>手术名称: {item.treatmentMethod}</p>
+                  <p>状态: {item.typeOfTutoring}</p>
+                  <p>金额: {`￥${item.cash}`}</p>
+                </>
+              </Timeline.Item>
+            );
+          })}
+        </Timeline>
+      ) : (
+        <Result status={404} title={'暂无消费记录'} subTitle={'消费记录暂时还没有哦'} />
+      )}
     </div>
-  )
+  );
 }
 
-export default Payment
+export default Payment;
